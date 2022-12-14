03e91663d1ed1781a2558a71c84dae9970ee5b57
Alireza Pishdar <pishdaralirza@gmail.com>
2022-12-14 11:04:54 +0330

    add created_at and updated_at for Todo model

--- todo/models.py
+++ todo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from user.models import User 
 
 
@@ -7,6 +8,8 @@ class Todo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todos")
     description = models.TextField(blank=True, default="")
     complete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title 
