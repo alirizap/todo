26b024b2532f68bb052cccc6e504f13855215273
Alireza Pishdar <pishdaralirza@gmail.com>
2022-12-14 11:16:30 +0330

    add category field to Todo model

--- todo/models.py
+++ todo/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-from django.utils import timezone
 from user.models import User 
+from category.models import Category
 
 
 class Todo(models.Model):
     title = models.CharField(max_length=50)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todos")
     description = models.TextField(blank=True, default="")
+    category = models.ManyToManyField(Category, related_name="categories", blank=True)
     complete = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
