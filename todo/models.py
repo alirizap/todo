962d12a6cd5cfc314ebcfb982ac8cd60892520d7
Alireza Pishdar <pishdaralirza@gmail.com>
2022-12-14 13:33:41 +0330

    remove uuid field from Todo model

--- todo/models.py
+++ todo/models.py
@@ -11,8 +11,6 @@ from category.models import Category
 class Todo(models.Model):
     title = models.CharField(max_length=50)
     slug = models.SlugField(unique=True, max_length=75, null=True, blank=True)
-    # uuid -> used by API to lookup todo record
-    uuid = models.UUIDField(db_index=True, default=uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todos")
     description = models.TextField(blank=True, default="")
     category = models.ManyToManyField(Category, related_name="categories", blank=True)
